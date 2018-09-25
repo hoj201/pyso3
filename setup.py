e6888dba39a54740bff41a8bfec88d85b2285380
hoj201 <hoj201@gmail.com>
2018-09-25 16:26:45 -0400

    synced release

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyso3",
-    version="0.0.2",
+    version="0.0.4",
     author="Henry Jacobs",
     author_email="hoj201@gmail.com",
     description="A minimal package for dealing with rotation matrices",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/hoj201/pyso3",
-    download_url="https://github.com/hoj201/pyso3/archive/v0.0.1.tar.gz",
+    download_url="https://github.com/hoj201/pyso3/archive/v0.0.4.tar.gz",
     packages=setuptools.find_packages(exclude=['test*']),
     classifiers=(
         "Programming Language :: Python :: 3",
